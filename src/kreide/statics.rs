35ba954e0d94e8cf15d3fdf1c49deb7e6330558f
hessiser <hessiser@gmail.com>
2025-04-10 06:03:53 -0400

    Refactoring

--- src/kreide/statics.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::LazyLock;
-
-// This is to keep track of static offsets that need to be manually updated
-pub static TEXTID_TYPE_PTR_OFFSET: LazyLock<usize> = lazy_initialize_address!(0x456CFC0);
-
-pub static GLOBAL_VARS_PTR_OFFSET: LazyLock<usize> = lazy_initialize_address!(0x425B9F0);
-
-pub static S_MODULEMANAGER_FIELD_OFFSET: isize = 0x365c0;
