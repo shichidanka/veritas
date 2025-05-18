e77f49222bcc3e64d33d5f28e4780ace43e3d46b
hessiser <hessiser@gmail.com>
2025-05-18 14:55:39 -0400

    Cleanup

--- src/kreide/functions.rs
+++ src/kreide/functions.rs
@@ -1,47 +1,73 @@
-use std::{ffi::c_void, sync::LazyLock};
-use crate::kreide::native_types::*;
-use crate::kreide::gamecore::*;
+#![allow(unused_imports)]
 use crate::kreide::client::*;
+use crate::kreide::gamecore::*;
+use crate::kreide::native_types::*;
 pub mod rpg {
-use std::{ffi::c_void, sync::LazyLock};
-use crate::kreide::native_types::*;
-use crate::kreide::gamecore::*;
-use crate::kreide::client::*;
-	pub mod gamecore {
-use std::{ffi::c_void, sync::LazyLock};
-use crate::kreide::native_types::*;
-use crate::kreide::gamecore::*;
-use crate::kreide::client::*;
-pub static AbilityStatic_GetActualOwner: LazyLock<fn(*const GameEntity) -> *const GameEntity> = lazy_initialize_address!(0x67483f0);
-pub static BattleEventSkillRowData_get_SkillName: LazyLock<fn(*const BattleEventSkillRowData) -> *const TextID> = lazy_initialize_address!(0x93cd310);
-pub static BattleEventSkillRowData_get_AttackType: LazyLock<fn(*const BattleEventSkillRowData) -> AttackType> = lazy_initialize_address!(0x93cd260);
-pub static EntityManager__GetEntitySummoner: LazyLock<fn(*const EntityManager,*const GameEntity) -> *const GameEntity> = lazy_initialize_address!(0x5976260);
-pub static ServantSkillRowData_get_SkillName: LazyLock<fn(*const ServantSkillRowData) -> *const TextID> = lazy_initialize_address!(0x5a82350);
-pub static ServantSkillRowData_get_AttackType: LazyLock<fn(*const ServantSkillRowData) -> AttackType> = lazy_initialize_address!(0x5a822a0);
-pub static TurnBasedAbilityComponent_GetAbilityMappedSkill: LazyLock<fn(*const TurnBasedAbilityComponent,*const NativeString) -> *const NativeString> = lazy_initialize_address!(0x5a9ae40);
-pub static AvatarSkillRowData_get_SkillName: LazyLock<fn(*const AvatarSkillRowData) -> *const TextID> = lazy_initialize_address!(0x93c5910);
-pub static AvatarSkillRowData_get_AttackType: LazyLock<fn(*const AvatarSkillRowData) -> AttackType> = lazy_initialize_address!(0x93c55e0);
-pub static SkillCharacterComponent_GetSkillData: LazyLock<fn(*const SkillCharacterComponent,i32,i32) -> *const SkillData> = lazy_initialize_address!(0x5a87470);
-pub static CharacterConfig_GetSkillIndexByTriggerKey: LazyLock<fn(*const CharacterConfig,*const NativeString) -> i32> = lazy_initialize_address!(0xe28bb30);
-pub static GamePlayStatic_GetEntityManager: LazyLock<fn() -> *const EntityManager> = lazy_initialize_address!(0x7f9c510);
-	}
-	pub mod client {
-use std::{ffi::c_void, sync::LazyLock};
-use crate::kreide::native_types::*;
-use crate::kreide::gamecore::*;
-use crate::kreide::client::*;
-pub static UIGameEntityUtils_GetAvatarID: LazyLock<fn(*const GameEntity) -> u32> = lazy_initialize_address!(0xae43d30);
-pub static AvatarModule_GetAvatar: LazyLock<fn(*const c_void,u32) -> *const AvatarData> = lazy_initialize_address!(0x9c53f00);
-pub static AvatarData_get_AvatarName: LazyLock<fn(*const AvatarData) -> *const NativeString> = lazy_initialize_address!(0xb3be890);
-pub static TextmapStatic_GetText: LazyLock<fn(*const TextID,*const NativeArray<NativeObject>) -> *const NativeString> = lazy_initialize_address!(0x83e6850);
-pub static GlobalVars_cctor: LazyLock<fn() -> *const NativeObject> = lazy_initialize_address!(0x9d94050);
-	}
+    pub mod gamecore {
+        use crate::kreide::client::*;
+        use crate::kreide::gamecore::*;
+        use crate::kreide::native_types::*;
+        use std::sync::LazyLock;
+        pub static AbilityStatic_GetActualOwner: LazyLock<
+            unsafe fn(*const GameEntity) -> *const GameEntity,
+        > = lazy_initialize_address!(0x67483f0);
+        pub static BattleEventSkillRowData_get_SkillName: LazyLock<
+            unsafe fn(*const BattleEventSkillRowData) -> *const TextID,
+        > = lazy_initialize_address!(0x93cd310);
+        pub static BattleEventSkillRowData_get_AttackType: LazyLock<
+            unsafe fn(*const BattleEventSkillRowData) -> AttackType,
+        > = lazy_initialize_address!(0x93cd260);
+        pub static EntityManager__GetEntitySummoner: LazyLock<
+            unsafe fn(*const EntityManager, *const GameEntity) -> *const GameEntity,
+        > = lazy_initialize_address!(0x5976260);
+        pub static ServantSkillRowData_get_SkillName: LazyLock<
+            unsafe fn(*const ServantSkillRowData) -> *const TextID,
+        > = lazy_initialize_address!(0x5a82350);
+        pub static ServantSkillRowData_get_AttackType: LazyLock<
+            unsafe fn(*const ServantSkillRowData) -> AttackType,
+        > = lazy_initialize_address!(0x5a822a0);
+        pub static TurnBasedAbilityComponent_GetAbilityMappedSkill: LazyLock<
+            unsafe fn(*const TurnBasedAbilityComponent, *const NativeString) -> *const NativeString,
+        > = lazy_initialize_address!(0x5a9ae40);
+        pub static AvatarSkillRowData_get_SkillName: LazyLock<
+            unsafe fn(*const AvatarSkillRowData) -> *const TextID,
+        > = lazy_initialize_address!(0x93c5910);
+        pub static AvatarSkillRowData_get_AttackType: LazyLock<
+            unsafe fn(*const AvatarSkillRowData) -> AttackType,
+        > = lazy_initialize_address!(0x93c55e0);
+        pub static SkillCharacterComponent_GetSkillData: LazyLock<
+            unsafe fn(*const SkillCharacterComponent, i32, i32) -> *const SkillData,
+        > = lazy_initialize_address!(0x5a87470);
+        pub static CharacterConfig_GetSkillIndexByTriggerKey: LazyLock<
+            unsafe fn(*const CharacterConfig, *const NativeString) -> i32,
+        > = lazy_initialize_address!(0xe28bb30);
+        pub static GamePlayStatic_GetEntityManager: LazyLock<unsafe fn() -> *const EntityManager> =
+            lazy_initialize_address!(0x7f9c510);
+    }
+    pub mod client {
+        use crate::kreide::client::*;
+        use crate::kreide::gamecore::*;
+        use crate::kreide::native_types::*;
+        use std::{ffi::c_void, sync::LazyLock};
+        pub static UIGameEntityUtils_GetAvatarID: LazyLock<unsafe fn(*const GameEntity) -> u32> =
+            lazy_initialize_address!(0xae43d30);
+        pub static AvatarModule_GetAvatar: LazyLock<
+            unsafe fn(*const c_void, u32) -> *const AvatarData,
+        > = lazy_initialize_address!(0x9c53f00);
+        pub static AvatarData_get_AvatarName: LazyLock<
+            unsafe fn(*const AvatarData) -> *const NativeString,
+        > = lazy_initialize_address!(0xb3be890);
+        pub static TextmapStatic_GetText: LazyLock<
+            unsafe fn(*const TextID, *const NativeArray<NativeObject>) -> *const NativeString,
+        > = lazy_initialize_address!(0x83e6850);
+        pub static GlobalVars_cctor: LazyLock<unsafe fn() -> *const NativeObject> =
+            lazy_initialize_address!(0x9d94050);
+    }
 }
 pub mod unityengine {
-use std::{ffi::c_void, sync::LazyLock};
-use crate::kreide::native_types::*;
-use crate::kreide::gamecore::*;
-use crate::kreide::client::*;
-pub static Application_set_targetFrameRate: LazyLock<fn(i32)> = lazy_initialize_address!(0xe5f94d0);
-pub static Application_get_targetFrameRate: LazyLock<fn() -> i32> = lazy_initialize_address!(0xe5f94c0);
+    use std::sync::LazyLock;
+    pub static Application_set_targetFrameRate: LazyLock<unsafe fn(i32)> =
+        lazy_initialize_address!(0xe5f94d0);
+    pub static Application_get_targetFrameRate: LazyLock<unsafe fn() -> i32> =
+        lazy_initialize_address!(0xe5f94c0);
 }
